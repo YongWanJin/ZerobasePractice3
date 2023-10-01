814fae8b889dd53e345ac012a62237c9481e1402
zzz <yongwan@naver.com>
2023-10-02 08:47:13 +0900

    2023-10-02 프로젝트 시작 및 초기화

---
+++ src/main/java/com/zerobase/Dividend/DividendApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.Dividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendApplication.class, args);
+	}
+
+}
